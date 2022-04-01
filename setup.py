cd9938e6f6bbcf0921f475994e5af2b6361acced
epalto <epalanquestost@mgh.harvard.edu>
2022-04-01 00:31:44 -0400

    fix entrypoint

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'facinfo = facinfo.scrap'
+            'facinfo = facinfo.scrap:main'
         ]
     },
     data_files=[('.', ['Pipfile']), ],
